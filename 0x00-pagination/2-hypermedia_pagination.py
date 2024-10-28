2ffc47027b0328e7e2c4c04aebc949a48944e827
Haidyasser <haidyasser3@gmail.com>
2024-10-28 18:27:26 +0300

    remove whitespace from a blank line

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -44,7 +44,7 @@ class Server:
 
         start, end = index_range(page, page_size)
         return self.dataset()[start:end]
-    
+
     def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
         """Return a dictionary containing the following key-value pairs
         """
