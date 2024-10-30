15ddb03e78ec6f47682cd879e34ee014b6a7a0be
Haidyasser <haidyasser3@gmail.com>
2024-10-30 20:58:51 +0300

    solve pycodestyle errors

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -14,7 +14,7 @@ class BasicCache(BaseCaching):
         """
         if key and item:
             self.cache_data[key] = item
-    
+
     def get(self, key):
         """ Get an item by key
         """
