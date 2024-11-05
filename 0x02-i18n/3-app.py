dadb8e9f9a72e4e1cf0e2ef5b388d52d68849b76
Haidyasser <haidyasser3@gmail.com>
2024-11-05 21:48:26 +0200

    task 3

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,32 +1,34 @@
 #!/usr/bin/env python3
-'''Basic app with only single route'''
+""" Basic Flask app """
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
-
-class Config:
-    '''Class for babel configuration'''
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
-
-
 app = Flask(__name__)
 babel = Babel(app)
+
+
+class Config:
+    """ Babel configuration """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
 app.config.from_object(Config)
 
 
 @babel.localeselector
 def get_locale():
-    '''Gets the locale language'''
+    """ Determines the best match with our
+    supported languages. """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
 def index():
-    '''Basic index page'''
-    return render_template('3-index.html')
+    """ Returns index """
+    return render_template('1-index.html')
 
 
 if __name__ == "__main__":
-    app.run(port="5000", host="0.0.0.0", debug=True)
+    app.run()
