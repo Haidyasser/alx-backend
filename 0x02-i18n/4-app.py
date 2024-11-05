7fc4b725a59452ec6e73573b6bf266eae32012e1
Haidyasser <haidyasser3@gmail.com>
2024-11-05 21:55:46 +0200

    task 4

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -20,14 +20,19 @@ app.config.from_object(Config)
 @babel.localeselector
 def get_locale():
     """ Determines the best match with our
-    supported languages. """
+    supported languages or forces a specific locale. """
+    # Check if a 'locale' parameter is present
+    # in the request
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
 def index():
     """ Returns index """
-    return render_template('3-index.html')
+    return render_template('4-index.html')
 
 
 if __name__ == "__main__":
