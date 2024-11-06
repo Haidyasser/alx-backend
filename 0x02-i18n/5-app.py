6c5d04c44a0060c2cde839af09634f93e4097862
Haidyasser <haidyasser3@gmail.com>
2024-11-06 12:31:48 +0200

    task 5

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -51,7 +51,7 @@ def before_request():
 @app.route('/')
 def index():
     """ Returns index """
-    return render_template('4-index.html')
+    return render_template('5-index.html')
 
 
 if __name__ == "__main__":
